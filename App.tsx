367c88b8031eaf7a51b5d36a867b1cb64c2b9a3f
Jesus Rodriguez <rodriguezjesus609@gmail.com>
2020-07-04 09:55:10 +0200

    Finalizar lista en home

--- App.tsx
+++ App.tsx
@@ -1,107 +1,69 @@
-import React from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  SafeAreaView,
-  ScrollView,
-  FlatList,
-  Image,
-} from "react-native";
+import React from 'react';
+import { StyleSheet, Text, View, Image, FlatList } from 'react-native';
 
-
-
-enum RestaurantType {
-  italianos,
-  argentino,
-  peruanos,
-  chileno,
-  venezolano,
-  colombiano,
-}
-
-class Restaurantes {
+class RestaurantType {
   id: string
-  tipo: RestaurantType
+  tipo: string
   image: string
-
-  constructor(
-    id: string,
-    tipo: RestaurantType,
-    image: string,
-  ) {
-    this.id = id;
-    this.tipo = tipo;
-    this.image = image;
+  constructor(id: string, tipo: string, image: string) {
+    this.id = id
+    this.tipo = tipo
+    this.image = image
   }
-  
 }
 
-const RESTAURANT_TYPES: Array<Restaurantes> = [
-  new Restaurantes("0", RestaurantType.argentino, require('./assets/argentino.jpg')),
-  new Restaurantes("1", RestaurantType.italianos, require('./assets/italiano.jpg')),
-  new Restaurantes("2", RestaurantType.peruanos, require('./assets/peruano.jpg')),
-];
+const dummyRestaurantTypes: Array<RestaurantType> = [
+  new RestaurantType("1", "Italiano", require('./assets/italiano.jpg')),
+  new RestaurantType("2", "Griego", require('./assets/griego.jpg')),
+  new RestaurantType("3", "Argentino", require('./assets/argentino.jpg')),
+  new RestaurantType("4", "Peruano", require('./assets/peruano.jpg'))
+]
 
-const renderItem = ({ item }: { item: Restaurantes }) => {
+const renderResturantTypes = ({ item } : { item: RestaurantType}) => {
   return (
-    <View style={styles.tarjeta}>
-      <Image style={styles.image} source={item.image} />
-      <Text style={styles.description}>
-        {`${RestaurantType[item.tipo]}`}
-      </Text> 
-    </View>
-  );
-};
+    <View style={styles.cards}>
+        <Image style={styles.image} source={item.image}/>
+        <Text style={styles.description}> {item.tipo} </Text>
+      </View>
+  )
+}
 
 export default function App() {
   return (
-    <SafeAreaView>
-      <ScrollView>
-        <View style={styles.container}>
-          <Text style={styles.header}>Restaurantes en tu area</Text>
-          <FlatList
-            style={styles.list}
-            data={RESTAURANT_TYPES}
-            renderItem={renderItem}
-          />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <Text style={styles.header}> Restaurantes en tu area </Text>
+      <FlatList style={styles.list} data={dummyRestaurantTypes} renderItem={renderResturantTypes} />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  header: {
-    marginTop: 40,
-    alignSelf: "flex-start",
-    fontSize: 24,
-  },
-  description: { 
-    fontSize: 16,
-    textTransform: "capitalize",
-    color: 'white',
-    backgroundColor: '#444',
-    padding: 10
-   },
-  list: {
-    marginTop: 20,
-    width: "100%",
-  },
-  tarjeta: {
-    borderRadius: 10,
-    padding: 10,
-    marginTop: 20,
-    borderColor: "black",
-  },
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-    marginHorizontal: 24,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  header: {
+    fontWeight: "bold",
+    fontSize: 24,
+    alignSelf: 'flex-start',
+    marginTop: 50
+  },
+  list: {
+    width: '100%'
+  },
+  cards: {
+    padding: 20
+  },
+  description: {
+    backgroundColor: '#444',
+    color: 'white',
+    padding: 10,
+    fontWeight: "bold"
   },
   image: {
-    width: '100%'
+    width: '100%',
+    height: 150
   }
 });
